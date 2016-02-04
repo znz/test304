13c2fe539564d1f86a8a0df453831a7c41f88352
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-02-04 17:43:02 +0900

    Add 2.times

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,8 +2,10 @@
 require 'rails_helper'
 
 describe 'Home pages', js: true do
-  it do
-    visit home_redirect_path
-    expect(page.status_code).to eq(200)
+  2.times do |n|
+    it "redirect (#{n})" do
+      visit home_redirect_path
+      expect(page.status_code).to eq(200)
+    end
   end
 end
