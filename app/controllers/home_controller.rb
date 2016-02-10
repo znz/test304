6d56b5fb25657c86033054a1d989f577b073c97f
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2016-02-10 18:25:39 +0900

    Add debug print

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    request.headers.each do |key, value|
+      puts "#{key}: #{value}" if /\AHTTP_/ =~ key
+    end
   end
 
   def redirect
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,6 +5,7 @@ describe 'Home pages', js: true do
   2.times do |n|
     it "redirect (#{n})" do
       visit home_redirect_path
+      p response_headers
       expect(page.status_code).to eq(200)
     end
   end
